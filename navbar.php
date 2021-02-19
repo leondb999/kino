79365b3ae978bcf8304780355f69bc237bc32fe0
NicolasS9501 <nicolas.alsfeld@outlook.de>
2021-02-19 12:23:47 +0100

    Navbar Nico5

--- navbar.php
+++ navbar.php
@@ -1,3 +1,4 @@
+<!--
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -37,6 +38,55 @@
   
 <div class="container">
   <h3>Hier kommt der Slider hin! HALLO!</h3>
+</div>
+
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+  <a class="navbar-brand" href="/kino/index.php">DHBW-Kino Mannheim</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kinoprogramm</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Über uns</a>
+      </li>
+    </ul>
+    <ul class="nav navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#"><button class="navbar-toggler" type="button"><span class="glyphicon glyphicon-log-in"></span></button></a>
+      </li>    
+      <li class="nav-item">
+        <a class="nav-link" href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Warenkorb</a>
+      </li>  
+    </ul>
+  </div>  
+</nav>
+<br>
+
+<div class="container">
+  <h3>Collapsible Navbar</h3>
+  <p>In this example, the navigation bar is hidden on small screens and replaced by a button in the top right corner (try to re-size this window).</p>
+  <p>Only when the button is clicked, the navigation bar will be displayed.</p>
+  <p>Tip: You can also remove the .navbar-expand-md class to ALWAYS hide navbar links and display the toggler button.</p>
 </div>
 
 </body>
