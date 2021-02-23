5b088aed01f109929b2d5c396f0ebbebb90b6056
NicolasS9501 <nicolas.alsfeld@outlook.de>
2021-02-23 18:49:38 +0100

    footer bar, impressum, bug fixes

--- index.php
+++ index.php
@@ -1,24 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <Title >Kino</Title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-
-  <!-- leon -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
   <link rel ="stylesheet"type="text/css" href="style.css"> 
-  <Title >Kino</Title>
 </head>
 <body>
   <!-- Import Variables and SQL Querys-->
@@ -27,8 +23,6 @@
   <?php include('./functions/slider-functions.php') ?>
   <?php include('./functions/database_config.php') ?>
 
-
-  <!-- Connect to MySQL DB-->
   <?php
     //connect to 
     $result_all_films = mysqli_query($con, "Select * from kinoticketing.film");
@@ -38,12 +32,11 @@
   ?>
 
 <header>
-  <!-- Navigation -->
   <?php include('./functions/navbar.php') ?>
 </header>                    	
 
   <!-- Slider --> 
-  <main role="main" style="padding-top: 0px">
+  <main role="main" style="padding-top: 0px; padding-bottom: 30px">
     <div id="top-film-carousel" class="carousel slide" data-ride="carousel"  data-interval ="1000">
         
         <!-- Navigations Striche -->
@@ -72,10 +65,9 @@
 
     <!-- Film Cards with PHP -->
     <section class="py-2 m-10">
-        <div class="container">
-            <h1 class="display-4">Kinoprogramm</h1>
-            <p class="lead">Take a look at our Kinoprogramm!</p>
-            <div class="row">
+        <div class="container" style="padding-top: 20px">
+            <h3 class="display-4">Kinoprogramm</h3>
+            <div class="row" style="padding-top: 20px">
                 <?php                    
                     while($film = mysqli_fetch_array($result_4_films))
                     {?>
@@ -92,21 +84,9 @@
             </div>
         </section>
   </main>
-  <!--
-  <footer class="footer">
-    <div class="container">
-      <span class="text-muted">Place sticky footer content here.</span>
-    </div>
-  </footer>
-  -->
-  <footer class="text-muted py-3 bg-dark">
-  <div class="container">
-    <p class="float-end mb-1">
-      <a href="#">Back to top</a>
-    </p>
-    <p class="mb-1">Album example is © Bootstrap, but please download and customize it for yourself!</p>
-    <p class="mb-1">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a href="/docs/5.0/getting-started/introduction/">getting started guide</a>.</p>
-  </div>
+  
+  <footer class="py-3 bg-dark" style="color: grey">
+    <?php include('./functions/footer.php') ?>
   </footer>
 
 </body>
